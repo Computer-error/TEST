5b3f58d010b0fdd2bc3c079bbda87f9dd1473075
Computer-error <valeroso.er@obmontessori.edu.ph>
2025-12-10 20:14:56 +0800

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,31 @@
+from pyscript import document
+
+def reveal(event):
+    grp = (document.getElementById("clb").value)
+     
+    if grp == "1":
+        club = "Math Club " \
+        "Learn more about numbers and shapes and join competitions! " \
+        "Meetings in Classroom 2-3 " \
+        ">Academic"
+    elif grp == "2":
+        club = "Science Club " \
+        "Master science therough experiments " \
+        "Meetings at Science Laboratory " \
+        ">Academic"
+    elif grp == "3":
+        club = "Band Club " \
+        "Make art through music and sounds and perform in events! " \
+        "Meetings at Music Room " \
+        ">Special"
+    elif grp == "4":
+        club = "Robotics Club " \
+        "Master the programming and create functional robots " \
+        "Meetings at Computer Room " \
+        ">Special"
+    elif grp == "5":
+        club = "Some clubs are not available yet, still under develoment."
+    else:
+        club = "Please select a club to view"
+        
+    document.getElementById("view").innerText = club
